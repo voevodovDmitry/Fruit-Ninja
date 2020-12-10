625946ed0f63a8f389cd807d6fa2351b24577e57
Dmitry Voevodov <mersisay@yandex.ru>
2020-12-11 01:23:09 +0300

    Parabolic motion
    Implemented parabolic motion for fruits

---
+++ Fruit-Ninja/Assets/Scripts/Fruit.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fruit : MonoBehaviour
+{
+    [SerializeField] int angle;
+    [SerializeField] float speedX = 10;
+    [SerializeField] float speedY = 10;
+    [SerializeField] float gravityValue = 10;
+
+    Vector3 speed = new Vector3(10, 10, 0);
+    Vector3 gravity = new Vector3(0, -10, 0);
+
+
+    
+
+    void Start()
+    {
+        InitSpeed();
+    }
+
+    
+    void Update()
+    {
+        speed += gravity * Time.deltaTime;         
+        transform.position += speed * Time.deltaTime;
+    }
+
+    void InitSpeed()
+    {
+
+        float myltipliyX = Mathf.Cos(angle * Mathf.PI / 180);
+        float myltipliyY = Mathf.Sin(angle * Mathf.PI / 180);
+        speed.x = speed.x * myltipliyX;
+        speed.y = speed.y * myltipliyY;       
+    }
+}
